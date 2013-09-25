070c8be5ef3cd7d5adbfeb8677fb2eca5515d614
Daniel Goree <daniel.botball@gmail.com>
2013-09-25 13:21:39 -0500

    Fixed comments, added randomize() moved all sort methods to Serial

--- Project2/src/Serial.java
+++ Project2/src/Serial.java
@@ -75,6 +75,21 @@ public class Serial implements Comparator<Paper>
      * Mutator Methods
      */
     
+	/**
+     * This randomizes the order of papers
+     * <P>
+     * Algorithm:<br>
+     * Algorithm not yet implemented.<br>
+     * </P>
+     * <dt><b>Conditions:</b>
+     * <dd>PRE  -         papers is not void
+     * <dd>POST -         papers is in a random order
+     */
+	public void randomize()
+	{
+		//do stuff
+	}
+	
     /**
      * This sorts papers by the last name of the primary author
      * <P>
@@ -149,11 +164,67 @@ public class Serial implements Comparator<Paper>
 	{
 		//do stuff
 	}
-	
-	/*
-     * Mutator Methods
-     */
     
+	 /**
+     * This sorts papers by their volume
+     * <P>
+     * Algorithm:<br>
+     * Algorithm not yet implemented.<br>
+     * </P>
+     * <dt><b>Conditions:</b>
+     * <dd>PRE  -         papers is not void
+     * <dd>POST -         papers is sorted by their volume
+     */
+	public void sortByVolume()
+	{
+		//do stuff
+	}
+	
+	 /**
+     * This sorts papers by their issue
+     * <P>
+     * Algorithm:<br>
+     * Algorithm not yet implemented.<br>
+     * </P>
+     * <dt><b>Conditions:</b>
+     * <dd>PRE  -         papers is not void
+     * <dd>POST -         papers is sorted by their issue
+     */
+	public void sortByIssue()
+	{
+		//do stuff
+	}
+	
+	/**
+     * This sorts papers by their length
+     * <P>
+     * Algorithm:<br>
+     * Algorithm not yet implemented.<br>
+     * </P>
+     * <dt><b>Conditions:</b>
+     * <dd>PRE  -         papers is not void
+     * <dd>POST -         papers is sorted by their length
+     */
+	public void sortByLength()
+	{
+		//do stuff
+	}
+	
+	/**
+     * This sorts papers by their first page
+     * <P>
+     * Algorithm:<br>
+     * Algorithm not yet implemented.<br>
+     * </P>
+     * <dt><b>Conditions:</b>
+     * <dd>PRE  -         papers is not void
+     * <dd>POST -         papers is sorted by their first page
+     */
+	public void sortByFirstPage()
+	{
+		//do stuff
+	}
+	
     /**
      * This adds a Paper to the ArrayList papers
      * <P>
@@ -167,19 +238,173 @@ public class Serial implements Comparator<Paper>
 	}
 	
 	
-	/** 
-	 * This compares two papers to one another
-	 * <P>
-	 * @param 			  o1		the first paper to be compared
-	 * @param			  o2		the second paper to be compared
-	 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where the primary author being alphanumerically before another is considered less than.
+	
+	public static Comparator<Paper>  authorComparator = new Comparator<Paper>() 
+	{
+		/** 
+		 * This compares two papers to one another by their author
+		 * <P>
+		 * @param 			  p1		the first paper to be compared
+		 * @param			  p2		the second paper to be compared
+		 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where the primary author being alphanumerically before another is considered less than.
+		 */
+		public int compare(Paper p1, Paper p2) 
+		{
+			//do Stuff
+			return 0;
+		}
+	};
+	
+	
+	public static Comparator<Paper>  titleComparator = new Comparator<Paper>() 
+	{
+		/** 
+		 * This compares two papers to one another by their title
+		 * <P>
+		 * @param 			  p1		the first paper to be compared
+		 * @param			  p2		the second paper to be compared
+		 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where the title being alphanumerically before another is considered less than.
+		 */
+		public int compare(Paper p1, Paper p2) 
+		{
+			//do Stuff
+			return 0;
+		}
+	};
+
+	public static Comparator<Paper>  dateComparator = new Comparator<Paper>() 
+	{
+		/** 
+		 * This compares two papers to one another by their date of publication
+		 * <P>
+		 * @param 			  p1		the first paper to be compared
+		 * @param			  p2		the second paper to be compared
+		 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where the date being before another is considered less than.
+		 */
+		public int compare(Paper p1, Paper p2) 
+		{
+			//do Stuff
+			return 0;
+		}
+	};
+
+	
+	public static Comparator<Paper>  typeComparator = new Comparator<Paper>() 
+	{
+		/** 
+		 * This compares two papers to one another by their type of Paper (Article/ConferencePaper)
+		 * <P>
+		 * @param 			  p1		the first paper to be compared
+		 * @param			  p2		the second paper to be compared
+		 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where an Article is considered less than a ConferencePaper.
+		 */
+		public int compare(Paper p1, Paper p2) 
+		{
+			//do Stuff
+			return 0;
+		}
+	};
+	
+
+	public static Comparator<Paper>  diComparator = new Comparator<Paper>() 
+	{	
+		/** 
+		 * This compares two papers to one another by their digital identifier
+		 * <P>
+		 * @param 			  p1		the first paper to be compared
+		 * @param			  p2		the second paper to be compared
+		 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where the digital identifier being alphanumerically before another is considered less than.
+		 */
+		public int compare(Paper p1, Paper p2) 
+		{
+			//do Stuff
+			return 0;
+		}
+	};
+	
+
+	public static Comparator<Paper>  volumeComparator = new Comparator<Paper>() 
+	{
+		/** 
+		 * This compares two papers to one another by their volume
+		 * <P>
+		 * @param 			  p1		the first paper to be compared
+		 * @param			  p2		the second paper to be compared
+		 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where a lower numbered volume is considered less than.
+		 */
+		public int compare(Paper p1, Paper p2) 
+		{
+			//do Stuff
+			return 0;
+		}
+	};
+	
+	
+	public static Comparator<Paper>  issueComparator = new Comparator<Paper>() 
+	{
+		/** 
+		 * This compares two papers to one another by their issue
+		 * <P>
+		 * @param 			  p1		the first paper to be compared
+		 * @param			  p2		the second paper to be compared
+		 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where a lower numbered issue is considered less than.
+		 */
+		public int compare(Paper p1, Paper p2) 
+		{
+			//do Stuff
+			return 0;
+		}
+	};
+	
+	
+	public static Comparator<Paper>  lengthComparator = new Comparator<Paper>() 
+	{
+		/** 
+		 * This compares two papers to one another by their length
+		 * <P>
+		 * @param 			  p1		the first paper to be compared
+		 * @param			  p2		the second paper to be compared
+		 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where a shorter length is considered less than.
+		 */
+		public int compare(Paper p1, Paper p2) 
+		{
+			//do Stuff
+			return 0;
+		}
+	};
+	
+	
+	public static Comparator<Paper>  firstPageComparator = new Comparator<Paper>() 
+	{
+		/** 
+		 * This compares two papers to one another by their first page
+		 * <P>
+		 * @param 			  p1		the first paper to be compared
+		 * @param			  p2		the second paper to be compared
+		 * @return			  -1, 0, or 1 as the o1 is less than, equal to, or greater than o2. Where the an earlier page is considered less than.
+		 */
+		public int compare(Paper p1, Paper p2) 
+		{
+			//do Stuff
+			return 0;
+		}
+	};
+	/*
+	 * this is a link that helped me figure out comparators. Hurrah for fruit analogies. 
+	 * http://www.mkyong.com/java/java-object-sorting-example-comparable-and-comparator/
 	 */
-	public int compare(Paper arg0, Paper arg1) {
+
+	
+	/** 
+	 * Honestly I'm not sure why this is needed, but the compiler needs it to exist to be happy.
+	 * <P>
+	 * @param 			  arg0		a Paper I don't plan to do anything with.
+	 * @param			  arg1		another Paper I don't plan to do anything with.
+	 * @return			  0
+	 */
+	public int compare(Paper arg0, Paper arg1) 
+	{
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
-	/*
-	 * http://www.mkyong.com/java/java-object-sorting-example-comparable-and-comparator/
-	 */
 }
