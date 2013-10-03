64ddd095cd4561ba59675d9620b3c1150b103943
Daniel Goree <daniel.botball@gmail.com>
2013-10-02 21:15:10 -0500

    fixed a warning

--- Project2/src/PublicationSystem.java
+++ Project2/src/PublicationSystem.java
@@ -1,18 +1,10 @@
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-
-import javax.swing.JOptionPane;
 
 /**
  * Project #2
